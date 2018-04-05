02f96b6b292c64fa5059e2133796b236bf228f76
Dipika Bhattacharya <dipika.bhattacharya@gmail.com>
2018-04-05 15:24:27 -0700

    Updated function doc to follow the Google Style

--- src/datetime/days_to_birthday.py
+++ src/datetime/days_to_birthday.py
@@ -7,26 +7,34 @@ class DateInFutureException(Exception):
 
 def parse_and_validate(user_input):
     """
-    Parse given string for a date, validate that the date is not in future,
-    and return a datetime object
+    Parses and validates given string for a date and returns a datetime object.
+    
+    Args:
+        user_input (str): The string to parse
+    
+    Returns:
+        birthday: A datetime object for the given date
+    
+    Raises:
+        ValueError: If the date format of the input string is invalid
+        DateInFutureException: If the date format is valid but the date is in the future
 
-    :param user_input string: The string to parse
-    :returns: datetime object for the given date
-    :raises: ValueError If the format is invalid
-    :raises: DateInFutureException If date is in the future
+    Examples:
+        A valid input string
+        >>> parse_and_validate('2012/07/23')
+        datetime.datetime(2012, 7, 23, 0, 0)
 
-    >>> parse_and_validate('2012/07/23')
-    datetime.datetime(2012, 7, 23, 0, 0)
+        Invalid input string - letters instead of a date
+        >>> parse_and_validate('abcd')
+        Traceback (most recent call last):
+        ...
+        ValueError: time data 'abcd' does not match format '%Y/%m/%d'
 
-    >>> parse_and_validate('abcd')
-    Traceback (most recent call last):
-    ...
-    ValueError: time data 'abcd' does not match format '%Y/%m/%d'
-
-    >>> parse_and_validate('2018/12/23')
-    Traceback (most recent call last):
-    ...
-    DateInFutureException: This birthday is in the future! Enter a valid date of birth.
+        A valid input string but date in future
+        >>> parse_and_validate('2018/12/23')
+        Traceback (most recent call last):
+        ...
+        DateInFutureException: This birthday is in the future! Enter a valid date of birth.
     """
     birthday = datetime.datetime.strptime(user_input, '%Y/%m/%d')
     
@@ -38,20 +46,35 @@ def parse_and_validate(user_input):
 
 def days_to_birthday(birthday):
     """
-    >>> days_to_birthday(datetime.datetime(1978, 7, 21))
-    (106, 40)
+    Returns number of days to a given birthday and the age of the person.
+    
+    Args:
+        birthday: A datetime object for the birthday
+    
+    Returns:
+        A tuple of (days, age)
+        days_to_birthday.days: Returns number of days remaining for the next birthday
+        user_age: Returns the age of the person with the given birthday
 
-    # earlier than current month
-    >>> days_to_birthday(datetime.datetime(1978, 1, 21))
-    (290, 41)
+    Note:
+        The doctests in the Examples section will fail in future because answers correspond to today's date.
 
-    # current month, earlier date
-    >>> days_to_birthday(datetime.datetime(1975, 4, 1))
-    (360, 44)
+    Examples:
+        A birthday in future
+        >>> days_to_birthday(datetime.datetime(1978, 7, 21))
+        (106, 40)
 
-    # current month, later date
-    >>> days_to_birthday(datetime.datetime(1975, 4, 21))
-    (15, 43)
+        A birthday in the past, same month
+        >>> days_to_birthday(datetime.datetime(1975, 4, 1))
+        (360, 44)
+
+        A birthday in future, same month
+        >>> days_to_birthday(datetime.datetime(1975, 4, 21))
+        (15, 43)
+
+        A birthday in future, another month
+        >>> days_to_birthday(datetime.datetime(1978, 1, 21))
+        (290, 41)
     """
 
     dt_today = datetime.datetime.today()
@@ -71,22 +94,28 @@ def days_to_birthday(birthday):
 
 
 if __name__ == "__main__":
-
     while True:
-        user_input = raw_input("What is your date of birth (yyyy/mm/dd): ")
-        try:
-            birthday = parse_and_validate(user_input)
-            break
-        except ValueError as e:
-            print e
-        except DateInFutureException as e:
-            print e
+        user_answer = raw_input("Find days remaining to birthday and age? (Y/N): ")
+        if user_answer.lower() == "y":
+            while True:
+                user_input = raw_input("What is your date of birth (yyyy/mm/dd): ")
+                try:
+                    birthday = parse_and_validate(user_input)
+                    break
+                except ValueError as e:
+                    print e
+                except DateInFutureException as e:
+                    print e
+                    
             
-    
-    (days, age) = days_to_birthday(birthday)
-    if days == 0:
-        print "Your birthday is today! You are {} years old today.".format(age)
-    else:
-        print "Your birthday is {} days away. You will be {} years old on your next birthday.".format(days, age)
+            (days, age) = days_to_birthday(birthday)
+            if days == 0:
+                print "The given birthday is today! The person is {} years old today.".format(age)
+            else:
+                print "The given birthday is {} days away. The person will be {} years old on his/her next birthday.".format(days, age)
+        elif user_answer == "n":
+            break
+        else:
+            print "Sorry, invalid input"
 
     
